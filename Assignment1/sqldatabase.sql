04b0b517188541b5b20af933fe44dffe03e5fa7e
MeetrajsinhSunva <146133938+MeetrajsinhSunva@users.noreply.github.com>
2024-02-13 20:22:36 -0800

    Add files via upload

---
+++ Assignment1/sqldatabase.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE my;
+USE my;
+CREATE TABLE emp(uid CHAR(30), uname CHAR(30));
+SELECT * FROM emp;
+INSERT INTO `my` (`uid`, `uname` ) VALUES
+('1', 'Meetraj'),
+('6623446', 'fbcvzcvb'),
+('1', 'Meetraj'),
+('15674', 'rnncnx'),
+('1356', 'cvddbbdb'),
+('1524523', 'cvbbzdf');
